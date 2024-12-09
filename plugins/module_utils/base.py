40d1c94b76b451589c571daa56671e524afec22c
mjannasch <mjannasch@scaleuptech.com>
2024-12-09 17:07:02 +0100

    fix future import

--- plugins/module_utils/base.py
+++ plugins/module_utils/base.py
@@ -2,8 +2,7 @@
 
 # Copyright: (c) 2024, Moritz Jannasch <mjannasch@scaleuptech.com>
 # GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
-
-from __future__ import absolute_import, annotations, division, print_function
+from __future__ import absolute_import, division, print_function
 
 
 __metaclass__ = type
