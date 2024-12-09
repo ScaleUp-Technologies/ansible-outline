4128e3bd3b7d71568f58faaefe0047715a0be703
mjannasch <mjannasch@scaleuptech.com>
2024-12-09 16:47:06 +0100

    add future imports

--- plugins/doc_fragments/outline_api.py
+++ plugins/doc_fragments/outline_api.py
@@ -1,4 +1,13 @@
-from __future__ import annotations
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2024, Moritz Jannasch <mjannasch@scaleuptech.com>
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
 
 
 class ModuleDocFragment(object):
--- plugins/module_utils/base.py
+++ plugins/module_utils/base.py
@@ -1,5 +1,13 @@
-from __future__ import annotations
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+# Copyright: (c) 2024, Moritz Jannasch <mjannasch@scaleuptech.com>
+# GNU General Public License v3.0+ (see LICENSE or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, annotations, division, print_function
+
+
+__metaclass__ = type
 
 argument_spec = dict(
     state=dict(choices=["present", "absent"], default="present"),
